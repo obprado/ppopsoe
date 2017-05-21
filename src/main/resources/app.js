e382cf1d83da80deac41488a7117491b19af113a
obprado <obprado@gmail.com>
2017-05-21 20:26:47 +0200

    chore(initproject) Added javascript to the application

---
+++ src/main/resources/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+        $("#PP").click(function(){
+            alert("Hola PP");
+        });
+        $("#PSOE").click(function(){
+            alert("Hola PSOE");
+        });
+    }
+);
