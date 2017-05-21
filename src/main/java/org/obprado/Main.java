af43b9aa66349a30cad9a8b3934672ad784e663b
obprado <obprado@gmail.com>
2017-05-21 19:17:23 +0200

    chore(initproject) Making the app serve files

--- src/main/java/org/obprado/Main.java
+++ src/main/java/org/obprado/Main.java
@@ -1,8 +1,35 @@
 package org.obprado;
 
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello PPSOE");
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        Server server = new Server(port);
+
+        ResourceHandler resourceHandler = htmlResourceHandler();
+        setHandler(server, resourceHandler);
+
+        server.start();
     }
+
+    private static void setHandler(Server server, ResourceHandler resourceHandler) {
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resourceHandler, new DefaultHandler() });
+        server.setHandler(handlers);
+    }
+
+    private static ResourceHandler htmlResourceHandler() {
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setWelcomeFiles(new String[]{ "index.html" });
+        resourceHandler.setResourceBase("src/main/resources/");
+        return resourceHandler;
+    }
+
 }
